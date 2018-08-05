c6eab15cd926143a02ea8db0394b193a4592b2ec
Kiril Vatev <vatev.1@gmail.com>
2018-08-05 14:02:44 -0400

    initial api objects

---
+++ index.js
@@ -0,0 +1,68 @@
+const puppeteer = require('puppeteer');
+
+const cache = function (func) {
+  let val;
+
+  return async function(...args) {
+    if (val) {
+      return val;
+    }
+
+    val = await func(...args);
+
+    return val;
+  };
+};
+
+const getBrowser = cache(async function () {
+  return await puppeteer.launch({
+    headless: false
+  });
+});
+
+const getPage = cache(async function () {
+  const browser = await getBrowser();
+  return await browser.newPage();
+}());
+
+async function goto(url) {
+  const page = getPage();
+  await page.goto(url);
+
+  return page;
+}
+
+async function getPosts(username) {
+  const page = await goto(`https://www.instagram.com/${username}/`);
+
+  const posts = await page.evaluate(() => {
+    const elems = document.querySelectorAll('[href*="/p/"]');
+    const links = [].slice.call(elems).map(el => el.href);
+    return links;
+  });
+
+  const ids = posts.map(p => {
+    return p.match(/\/p\/([^/]+)/)[1];
+  });
+
+  return ids;
+}
+
+async function getStats(post) {
+  const page = await goto(`https://www.instagram.com/p/${post}/`);
+
+  const likes = await page.$$eval('[role=button]', els => els.filter(el => /[0-9]+ likes/.test(el.innerText)).map(el => el.innerText)[0]);
+  const hashtags = await page.$$eval('[href*="/explore/tags"]', els => els.map(el => el.innerText));
+
+  return {
+    likes: likes.match(/^([0-9]+)/)[1],
+    hashtags
+  };
+}
+
+module.exports = {
+  getBrowser,
+  goto,
+  getPosts,
+  getStats
+};
