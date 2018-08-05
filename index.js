6771256c968169495c6d9fa838f12282bbaa07ff
Kiril Vatev <vatev.1@gmail.com>
2018-08-05 15:27:50 -0400

    adding links to posts and a done method to close the browser

--- index.js
+++ index.js
@@ -49,22 +49,31 @@ async function getPosts(username) {
 }
 
 async function getStats(post) {
-  const page = await goto(`https://www.instagram.com/p/${post}/`);
+  const link = `https://www.instagram.com/p/${post}/`;
+  const page = await goto(link);
 
   const likes = await page.$$eval('[role=button]', els => els.filter(el => /[0-9]+ likes/.test(el.innerText)).map(el => el.innerText)[0]);
   const hashtags = await page.$$eval('[href*="/explore/tags"]', els => els.map(el => el.innerText));
   const datetime = await page.$eval('[datetime]', el => el.getAttribute('datetime'));
 
   return {
+    post,
+    link,
     likes: likes.match(/^([0-9]+)/)[1],
     hashtags,
     datetime
   };
 }
 
+async function done() {
+  const browser = await getBrowser();
+  await browser.close();
+}
+
 module.exports = {
   getBrowser,
   goto,
   getPosts,
-  getStats
+  getStats,
+  done
 };
